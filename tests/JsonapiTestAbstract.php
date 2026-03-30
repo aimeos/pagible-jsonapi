66baea029154612126efafe6357009dae353a88b
aimeos <aimeos@aimeos.org>
2026-03-30 09:19:12 +0000

    Simplified PHP code (#136)

--- tests/JsonapiTestAbstract.php
+++ tests/JsonapiTestAbstract.php
@@ -7,55 +7,13 @@
 
 namespace Tests;
 
-use Illuminate\Foundation\Testing\Concerns\InteractsWithViews;
-use Orchestra\Testbench\Concerns\WithLaravelMigrations;
 
-
-abstract class JsonapiTestAbstract extends \Orchestra\Testbench\TestCase
+abstract class JsonapiTestAbstract extends CmsTestAbstract
 {
-    use InteractsWithViews;
-    use WithLaravelMigrations;
-
-
-    protected ?\App\Models\User $user = null;
-    protected $enablesPackageDiscoveries = true;
-
-
-    protected function defineDatabaseMigrations()
-    {
-        \Orchestra\Testbench\after_resolving($this->app, 'migrator', static function ($migrator) {
-            $migrator->path(\Orchestra\Testbench\default_migration_path());
-        });
-    }
-
-
-	protected function defineEnvironment( $app )
-	{
-        $app['config']->set('database.connections.testing', [
-            'driver'   => env('DB_DRIVER', 'sqlite'),
-            'host'     => env('DB_HOST', ''),
-            'port'     => env('DB_PORT', ''),
-            'database' => env('DB_DATABASE', ':memory:'),
-            'username' => env('DB_USERNAME', ''),
-            'password' => env('DB_PASSWORD', ''),
-        ]);
-
-        $app['config']->set('auth.providers.users.model', 'App\\Models\\User');
-        $app['config']->set('cms.db', 'testing');
-        $app['config']->set('scout.driver', 'null');
-
-        \Aimeos\Cms\Tenancy::$callback = function() {
-            return 'test';
-        };
-    }
-
-
 	protected function getPackageProviders( $app )
 	{
-		return [
-			'Aimeos\Cms\CoreServiceProvider',
+		return array_merge( parent::getPackageProviders( $app ), [
 			'Aimeos\Cms\JsonapiServiceProvider',
-			'Aimeos\Nestedset\NestedSetServiceProvider',
-		];
+		] );
 	}
 }
